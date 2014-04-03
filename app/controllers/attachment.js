6b079e442cc9c0eab51b54f516b8b500a8c2a671
Nick Nance <nance.nick@gmail.com>
2014-04-02 23:25:41 -0500

    add file storage to amazon s3

--- app/controllers/attachment.js
+++ app/controllers/attachment.js
@@ -1,20 +1,34 @@
-var fs 		= require('fs');
+var fs 		= require('fs'),
+	AWS 	= require('aws-sdk'),
 	config 	= require('../../config/config');
 
 exports.saveAttachment = function(req, res){
 	console.log('controller/attachment post: ' + req.files[0].path );
 
-	var newLocation = config.root + '/public/files/' + req.files[0].name;
-	console.log('controller/attachment newLocation: ' + newLocation);
+	// var newLocation = config.root + '/public/files/' + req.files[0].name;
+	// console.log('controller/attachment newLocation: ' + newLocation);
 
-	fs.rename(req.files[0].path, newLocation, function (err) {
-		if (err) {
-			throw err;
-		} else {
-			res.send({
-				path: newLocation,
-				fileName: req.files[0].name
-			});			
-		}
+	fs.readFile(req.files[0].path, function (err, data) {
+
+ 		var bucket = 'wmga';
+
+		var s3 = new AWS.S3();
+		s3.putObject({
+			ACL: 'public-read', // by default private access
+			Bucket: bucket,
+			Key: req.files[0].name,
+			Body: data
+		}, function (err, data) {
+			if (err) {
+				console.log(err);
+				res.send(500, {msg: 'image upload failed', error: err})
+			} else {
+				console.log('S3 upload Successful');
+				res.send({
+					path: newLocation,
+					fileName: req.files[0].name
+				});			
+			}
+		});
 	});
 };
